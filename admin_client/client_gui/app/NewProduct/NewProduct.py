da6ef7cf16386de84de2237c7530ae2fec3310d1
kohmet <k.j.hirner.wisdom@gmail.com>
2020-05-15 08:48:34 -0700

    updates to add EntityStackModule as a generic stand-in for deletion types

--- admin_client/client_gui/app/NewProduct/NewProduct.py
+++ admin_client/client_gui/app/NewProduct/NewProduct.py
@@ -41,6 +41,41 @@ class NewProduct(QWidget):
         self.departmentWorker.signals.hasItem.connect(self.addToCombo)
         self.qtp.start(self.departmentWorker)
 
+        self.widget.save.clicked.connect(self.construct_and_send) 
+
+    def construct_and_send(self):
+        newProductData=self.constructNewProductData()
+        #make new product worker
+        #get product id
+        product_id=2
+        manufacturer_uri=self.constructUpdateAdd(self.widget.manufacturer,"product",product_id)
+        vendor_uri=self.constructUpdateAdd(self.widget.vendor,"product",product_id)
+        brand_uri=self.constructUpdateAdd(self.widget.brand,"product",product_id)
+        department_uri=self.constructUpdateAdd(self.widget.department,"product",product_id)
+
+        print(department_uri,brand_uri,vendor_uri,manufacturer_uri,sep="\n")
+        
+
+    def constructUpdateAdd(self,combobox,WHAT,product_id) -> str:
+        text=combobox.currentText()
+        ID=text.split("-")[0]
+        url="{address}/{WHAT}/update/{product_id}/add/{WHO}/{ID}".format(address=self.auth.get("server_address"),WHAT=WHAT,ID=ID,product_id=product_id,WHO=combobox.objectName())
+        return url
+
+    def constructNewProductData(self) -> dict:
+        payload=dict(
+            upc=self.widget.upc.text(),
+            homecode=self.widget.homecode.text(),
+            name=self.widget.name.text(),
+            casecount=self.widget.casecount.value(),
+            comment=self.widget.comment.toPlainText(),
+            priceUnit=self.widget.priceUnit.currentText(),
+            price=self.widget.price.value(),
+            weightUnit=self.widget.weightUnit.currentText(),
+            weight=self.widget.weight.value()
+        )
+        return payload
+
     def addToCombo(self,combo,data):
         print(data,combo)
         combo.addItem("{id} - {name}".format(**data))
