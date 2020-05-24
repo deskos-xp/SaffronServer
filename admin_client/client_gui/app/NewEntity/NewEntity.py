667e8495f2833636edcca073a3626d903210a5b3
noguilinux <k.j.hirner.wisdom@gmail.com>
2020-05-24 09:15:47 -0700

    updated new entity dialog

--- admin_client/client_gui/app/NewEntity/NewEntity.py
+++ admin_client/client_gui/app/NewEntity/NewEntity.py
@@ -6,15 +6,18 @@ import os,sys,json,ast,requests
 from .NewEntityListModel import NewEntityListModel
 from .NewEntityTableModel import NewEntityTableModel
 from .workers.CommitToServer import CommitToServer
+from .workers.GetComboData import GetComboData
+
 class SaveEntity(QObject):
     updateAll:pyqtSignal=pyqtSignal()
-    def __init__(self,obj,auth:dict,TYPE:str):
+    complete:pyqtSignal=pyqtSignal()
+    def __init__(self,obj,auth:dict,TYPE:str,parent,dialog):
         super(SaveEntity,self).__init__()
         self.obj=obj
         self.auth=auth
         self.TYPE=TYPE
-        model=obj.model()
-        print(model)
+        self.parent=parent
+        self.dialog=dialog
 
     @pyqtSlot()
     def update(self):
@@ -41,17 +44,54 @@ class SaveEntity(QObject):
     @pyqtSlot(requests.Response)
     def displayResponse(self,response):
         print(response)
+        self.complete.emit()
+        self.parent.progressBar.hide()
 
     @pyqtSlot(bool)
     def display(self,state):
+        self.parent.progressBar.show()
         print(self.obj.model().item)
         self.createWorker()
         self.commission()
+
+    @pyqtSlot(bool)
+    def resetView(self,state):
+        self.obj.model().load_data(NewEntity.fields(self,self.TYPE))
+        self.obj.model().layoutChanged.emit()
+
+    @pyqtSlot(bool)
+    def handle_new_address(self,state):
+        print(self.sender().parent().parent().parent().objectName(),state)
+        self.next()
+ 
+    @pyqtSlot(bool)
+    def status_first(self,state):
+        print(self.TYPE)
+
+    def back(self):
+        self.dialog.stackedWidget.setCurrentIndex(0)
+    
+    def next(self):
+        self.dialog.stackedWidget.setCurrentIndex(1)
+
+    '''
+    if addresses combo has a selected address:
+        do update
+    else:
+        pass
+    '''
+    ####
+    '''for saving address:
+            get id after save
+            do update method for TYPE
+    '''
 
 class NewEntity(QDialog):
     @pyqtSlot()
     def update(self):
         self.parent.newGrid.initialize(re=True)
+        for i in self.types:
+            self.prepCombos(i,re=True)
 
     def __init__(self,auth:dict,parent):
         super(NewEntity,self).__init__()
@@ -74,19 +114,43 @@ class NewEntity(QDialog):
         self.dialog.confirm.rejected.connect(self.dialog.reject)
         self.savers=dict()
 
+        def back(state):
+            SaveEntity.back(self)
+
+        self.comboWorkers=dict()
+
         self.models=dict()
+        self.address_tmp=dict()
+
+        for i in self.types:
+            self.address_tmp[i]=list()
+            self.comboWorkers[i]=GetComboData(self.auth,'address')
+
+        self.prepCombos("vendor")
+        self.prepCombos("brand")
+        self.prepCombos("manufacturer")
+
+
         for i in self.types:
             uic.loadUi("app/NewEntity/forms/NewEntityItem.ui",getattr(self.dialog,i))
             getattr(self.dialog,i).progressBar.hide()
             self.models[i]=NewEntityTableModel()
-            self.savers[i]=SaveEntity(getattr(self.dialog,i).tableView,self.auth,i)
+            self.savers[i]=SaveEntity(getattr(self.dialog,i).tableView,self.auth,i,getattr(self.dialog,i),self.dialog)
             self.savers[i].updateAll.connect(self.update)
+            
+            if i not in  ['address','department']:
+                pass
+            else:
+                getattr(self.dialog,i).addresses.setEnabled(False)
+                getattr(self.dialog,i).addresses.hide()
 
+            getattr(self.dialog,i).setObjectName(i)
+            #worker address updater
             getattr(self.dialog,i).tableView.setModel(self.models[i])
             getattr(self.dialog,i).tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
             getattr(self.dialog,i).tableView.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
             getattr(self.dialog,i).save.clicked.connect(self.savers[i].display)
-
+            getattr(self.dialog,i).clear.clicked.connect(self.savers[i].resetView)
             self.models[i].load_data(self.fields(i))
             self.models[i].layoutChanged.emit()
 
@@ -94,6 +158,30 @@ class NewEntity(QDialog):
         #need progress bar
 
         self.dialog.exec_()
+        
+    def prepCombos(self,name,re=False):
+        if re == True:
+            self.address_tmp[name]=list()
+            self.comboWorkers[name]=GetComboData(self.auth,'address')
+
+        self.comboWorkers[name].signals.hasItems.connect(lambda x,n: self.updateCombo(name,x))
+        self.comboWorkers[name].signals.hasError.connect(lambda x: print(x))
+        QThreadPool.globalInstance().start(self.comboWorkers[name])
+
+
+    def toAddressString(self,data:dict):
+        #"{'ZIP': '', 'apartment_suite': '', 'city': '', 'id': 6, 'state': '', 'street_name': '', 'street_number': '', 'name': ' , ,  '"
+        return "{id} - {street_number} {street_name}, {city}, {state} {ZIP} ({apartment_suite})".format(**data)
+
+    def updateCombo(self,name,data):
+        combo=getattr(self.dialog,name)
+        print(combo.objectName())
+        if self.toAddressString(data) not in self.address_tmp[name]:
+            self.address_tmp[name].append(self.toAddressString(data))
+        for x in self.address_tmp[name]:
+            getattr(self.dialog,name).addresses.addItem(x)
+        #combo.addItems(self.address_tmp[i])
+
 
     def fields(self,name):
         def addressFields():
---
+++ admin_client/client_gui/app/NewEntity/workers/GetComboData.py
@@ -0,0 +1,41 @@
+from PyQt5.QtCore import QObject,QRunnable,QThreadPool,QThread,pyqtSignal,pyqtSlot
+import ast,requests,os,sys
+
+class GetComboDataSignals(QObject):
+    killMe:bool=False
+    hasItems:pyqtSignal=pyqtSignal(dict,str)
+    hasError:pyqtSignal=pyqtSignal(Exception)    
+    finish:pyqtSignal=pyqtSignal()
+    session:requests.Session=requests.Session()
+    
+    @pyqtSlot()
+    def kill(self):
+        self.killMe=True
+        self.session.close()
+
+class GetComboData(QRunnable):
+    def __init__(self,auth:dict,name:str):
+        super(GetComboData,self).__init__()
+        self.signals=GetComboDataSignals()
+        self.auth=auth
+        self.name=name
+
+    def run(self):
+        try:
+            address="{address}/{name}/get".format(**dict(address=self.auth['server_address'],name=self.name))
+            postable=dict(limit=sys.maxsize,page=0)
+            session=self.signals.session.post(address,json=postable,auth=(self.auth.get("username"),self.auth.get("password")))
+            if session.status_code == 200:
+                if 'status' in session.json().keys():
+                    d=session.json().get(session.json().get('status'))
+                    for i in d:
+                        if self.name == "address":
+                            i['name']="{street_number} {street_name}, {city}, {state} {ZIP}".format(**i)
+                        #i['name']="{TYPE}:{ID}"
+                        self.signals.hasItems.emit(i,self.name)
+            print(address)
+            #self.signals.session.post()
+        except Exception as e:
+            print(e)
+            self.signals.hasError.emit(e)
+        self.signals.finish.emit()            
