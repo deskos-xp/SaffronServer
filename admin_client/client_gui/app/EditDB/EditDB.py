5510e50df8ae0eeaff10090f2de56e50f3db62be
noguilinux <k.j.hirner.wisdom@gmail.com>
2020-05-26 10:26:38 -0700

    completed functionality for searching in edit db dialog, now new gui components are need

--- admin_client/client_gui/app/EditDB/EditDB.py
+++ admin_client/client_gui/app/EditDB/EditDB.py
@@ -24,16 +24,15 @@ class EditDB(QDialog):
             "department",
             "product"
                 )
-
+        #QDialog.u
         self.searchModel=EditDBListModel(items=widgetNames)
         self.dialog.listView.setModel(self.searchModel)
         self.dialog.listView.activated.connect(self.switch_stack)
-        
         self.stackedWidgetsListModels=dict()
         self.stackedWidgetsTableModels=dict() 
         self.worker=dict()
         self.stackedWidgets=dict()
-
+        
         for wn in widgetNames:
             self.stackedWidgets[wn]=QWidget()
             self.dialog.stackedWidget.addWidget(self.stackedWidgets[wn])
@@ -58,33 +57,62 @@ class EditDB(QDialog):
         self.stackedWidgetsTableModels[wn].load_data(self.fields(wn))
         self.stackedWidgetsTableModels[wn].layoutChanged.emit()
         def SearchWorkerRun():
+            clear()
             self.SearchWorkerRun(wn)
 
-        def switchToTab():
-            #self.application.
+        def switchToTab(index):
             for i in range(self.dialog.application.count()):
                 if self.dialog.application.tabText(i).lower() == wn:
                     self.dialog.application.setCurrentIndex(i)
+                    selectedData=self.stackedWidgetsListModels[wn].items[index.row()]
+                    print(selectedData)
 
         def inc(state):
             self.stackedWidgets[wn].page.setValue(self.stackedWidgets[wn].page.value()+1)
-            self.SearchWorkerRun(wn)
+            #self.SearchWorkerRun(wn)
 
         def dec(state):
             self.stackedWidgets[wn].page.setValue(self.stackedWidgets[wn].page.value()-1)
+            #self.SearchWorkerRun(wn)
+        
+        
+        def onValueChanged(state):
+            if self.sender().objectName() == "page":
+                if self.sender().value() < 1:
+                    self.sender().setValue(0)
+                    self.stackedWidgets[wn].back.setEnabled(False)
+                else:
+                    self.stackedWidgets[wn].back.setEnabled(True)
             self.SearchWorkerRun(wn)
+
+        def clear():
+            self.stackedWidgetsTableModels[wn].load_data(self.fields(wn))
+            self.stackedWidgetsTableModels[wn].layoutChanged.emit()
+            self.stackedWidgetsListModels[wn].items.clear()
+            self.stackedWidgetsListModels[wn].layoutChanged.emit()
+
+        self.stackedWidgets[wn].page.valueChanged.connect(onValueChanged)
+        self.stackedWidgets[wn].limit.valueChanged.connect(onValueChanged)
 
         self.stackedWidgets[wn].search.clicked.connect(SearchWorkerRun)
         self.stackedWidgets[wn].back.clicked.connect(dec)
         self.stackedWidgets[wn].next.clicked.connect(inc)
+        self.stackedWidgets[wn].clear.clicked.connect(clear)
         self.stackedWidgets[wn].results.activated.connect(switchToTab)
 
-
-
-    def SearchWorkerRun(self,wn):
+    def SearchWorkerRun(self,wn,preSearch=None):
         def updateModel(data):
             self.stackedWidgetsListModels[wn].items.clear()
             self.stackedWidgetsListModels[wn].layoutChanged.emit()
+            print(data,"tag"*4)
+            if data in [[],{},None] or type(data) == type(Exception()):
+                self.stackedWidgets[wn].next.setEnabled(False)
+                if self.stackedWidgets[wn].page.value() >= 1:
+                    self.stackedWidgets[wn].page.setValue(self.stackedWidgets[wn].page.value()-1)
+                if type(data) == type(Exception()):
+                    return
+            else:
+                self.stackedWidgets[wn].next.setEnabled(True)
             if type(data) == type(dict()):
                 self.stackedWidgetsListModels[wn].items.append(data)
                 self.stackedWidgetsListModels[wn].layoutChanged.emit()
@@ -98,9 +126,11 @@ class EditDB(QDialog):
         data=self.stackedWidgetsTableModels[wn].dataToItem()
         data['page']=self.stackedWidgets[wn].page.value()
         data['limit']=self.stackedWidgets[wn].limit.value()
+        if preSearch != None:
+            data=preSearch(data)
         self.worker[wn]['search']=SearchWorker(self.auth,data,wn,self.fields(wn))
-        self.worker[wn]['search'].signals.hasError.connect(lambda e:print(e))
-        self.worker[wn]['search'].signals.hasItem.connect(lambda e:print(e))
+        self.worker[wn]['search'].signals.hasError.connect(updateModel)
+        self.worker[wn]['search'].signals.hasItem.connect(updateModel)
         self.worker[wn]['search'].signals.hasItems.connect(updateModel)
         self.worker[wn]['search'].signals.finished.connect(lambda : print("search finished!"))
 
@@ -119,6 +149,87 @@ class EditDB(QDialog):
         self.stackedWidgetsTableModels[wn].layoutChanged.emit()
         self.stackedWidgets[wn].who.setText(wn[0].upper()+wn[1:])
         self.worker[wn]=dict()
+
+
+        def stateOfInclusion(state):
+            #print(state)
+            toChange=self.sender().objectName().replace("use","")
+            if toChange.lower() == "casecount":
+                toChange=toChange.lower()
+            else:
+                toChange=toChange[0].lower()+toChange[1:]
+            getattr(self.stackedWidgets[wn],toChange).setEnabled(state)
+
+        def SearchWorkerRun():
+            clear()
+            self.SearchWorkerRun(wn,preSearch=preSearch)
+
+        def switchToTab(index):
+            for i in range(self.dialog.application.count()):
+                if self.dialog.application.tabText(i).lower() == wn:
+                    self.dialog.application.setCurrentIndex(i)
+                    selectedData=dict(self.stackedWidgetsListModels[wn].items[index.row()])
+                    print(selectedData)
+
+        def preSearch(searchAbleData): 
+            if self.stackedWidgets[wn].weight.isEnabled():
+                res=self.stackedWidgets[wn].weight.value()
+                if res.is_integer():
+                    res=int(res)
+                searchAbleData['weight']=res
+            if self.stackedWidgets[wn].price.isEnabled():
+                res=self.stackedWidgets[wn].price.value()
+                if res.is_integer():
+                    res=int(res)
+                searchAbleData['price']=res
+            if self.stackedWidgets[wn].weightUnit.isEnabled():
+                searchAbleData['weightUnit']=self.stackedWidgets[wn].weightUnit.currentText()
+            if self.stackedWidgets[wn].priceUnit.isEnabled():
+                searchAbleData['priceUnit']=self.stackedWidgets[wn].priceUnit.currentText()
+            print(searchAbleData)
+            if self.stackedWidgets[wn].casecount.isEnabled():
+                searchAbleData['case_count']=self.stackedWidgets[wn].casecount.value()
+
+            return searchAbleData 
+
+        def inc(state):
+            self.stackedWidgets[wn].page.setValue(self.stackedWidgets[wn].page.value()+1)
+            #self.SearchWorkerRun(wn)
+
+        def dec(state):
+            self.stackedWidgets[wn].page.setValue(self.stackedWidgets[wn].page.value()-1)
+            #self.SearchWorkerRun(wn)
+        
+        
+        def onValueChanged(state):
+            if self.sender().objectName() == "page":
+                if self.sender().value() < 1:
+                    self.sender().setValue(0)
+                    self.stackedWidgets[wn].back.setEnabled(False)
+                else:
+                    self.stackedWidgets[wn].back.setEnabled(True)
+            SearchWorkerRun()
+
+        def clear():
+            self.stackedWidgetsTableModels[wn].load_data(self.fields(wn))
+            self.stackedWidgetsTableModels[wn].layoutChanged.emit()
+            self.stackedWidgetsListModels[wn].items.clear()
+            self.stackedWidgetsListModels[wn].layoutChanged.emit()
+
+        self.stackedWidgets[wn].page.valueChanged.connect(onValueChanged)
+        self.stackedWidgets[wn].limit.valueChanged.connect(onValueChanged)
+
+        self.stackedWidgets[wn].search.clicked.connect(SearchWorkerRun)
+        self.stackedWidgets[wn].back.clicked.connect(dec)
+        self.stackedWidgets[wn].next.clicked.connect(inc)
+        self.stackedWidgets[wn].clear.clicked.connect(clear)
+        self.stackedWidgets[wn].results.activated.connect(switchToTab)
+
+        self.stackedWidgets[wn].usePriceUnit.toggled.connect(stateOfInclusion)
+        self.stackedWidgets[wn].usePrice.toggled.connect(stateOfInclusion)
+        self.stackedWidgets[wn].useWeightUnit.toggled.connect(stateOfInclusion)
+        self.stackedWidgets[wn].useWeight.toggled.connect(stateOfInclusion)
+        self.stackedWidgets[wn].useCaseCount.toggled.connect(stateOfInclusion)
 
 
     def productWorkersComplex(self,re=False,wn='product'):
