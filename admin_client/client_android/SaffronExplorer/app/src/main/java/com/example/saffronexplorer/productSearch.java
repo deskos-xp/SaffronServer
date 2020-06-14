233d1e9bc8e5e640a1581ba6337d0e52b86e16ef
noguilinux <k.j.hirner.wisdom@gmail.com>
2020-06-13 22:18:47 -0700

    updates

---
+++ admin_client/client_android/SaffronExplorer/app/src/main/java/com/example/saffronexplorer/productSearch.java
@@ -0,0 +1,94 @@
+package com.example.saffronexplorer;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import clients.Callable;
+import clients.Client;
+import entities.User;
+import pages.ProductPages;
+
+public class productSearch extends AppCompatActivity {
+    private String fieldsToJson(View view){
+        EditText name=findViewById(R.id.name);
+        EditText homecode=findViewById(R.id.homecode);
+        EditText upc=findViewById(R.id.upc);
+        EditText ID=findViewById(R.id.id);
+        HashMap toBe = new HashMap<>();
+        if (!name.getText().toString().equals("")) {
+            toBe.put("name", name.getText().toString());
+        }
+        if (!homecode.getText().toString().equals("")) {
+            toBe.put("home_code", homecode.getText().toString());
+        }
+        if (!upc.getText().toString().equals("")) {
+            toBe.put("upc", upc.getText().toString());
+        }
+        if (!ID.getText().toString().equals("")) {
+            toBe.put("id", Integer.parseInt(ID.getText().toString()));
+        }
+        toBe.put("page",0);
+        toBe.put("limit",55);
+        return new Gson().toJson(toBe);
+    }
+
+    private void search(String json) throws Exception {
+        Intent intent = getIntent();
+        User u = new Gson().fromJson(intent.getStringExtra("user"),User.class);
+        Client client = new Client();
+        ProductPages productPages = (ProductPages) client.client(intent.getStringExtra("host"),u.getUname(),u.getPassword(),ProductPages.class);
+        client.get(productPages.get_all(json), productPages, new Callable() {
+            @Override
+            public void call(Object o) {
+
+            }
+
+            @Override
+            public void call() {
+
+            }
+
+            @Override
+            public void call(Object o, Integer integer) {
+                Toast.makeText(getBaseContext(),String.valueOf(integer),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //getIntent().getStringExtra("user")
+                try {
+                    search(fieldsToJson(view));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Snackbar.make(view,fieldsToJson(view) , Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
